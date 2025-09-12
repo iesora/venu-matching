958b1fb971e7f24d0112ee8dce73ff87b51b0c8b
larcenciel618 <hhiirroossee816@gmail.com>
2025-09-12 19:12:22 +0900

    create apis for creatorEvent

--- api/src/modules/event/event.controller.ts
+++ api/src/modules/event/event.controller.ts
@@ -1,17 +1,57 @@
-import { Controller, Get, Param } from "@nestjs/common";
-import { EventService } from "./event.service";
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { EventService } from './event.service';
 
-@Controller("event")
+export interface CreateEventDto {
+  title: string;
+  description: string;
+  startDate: Date;
+  endDate: Date;
+  venueId: number;
+  creatorIds: number[];
+}
+
+export interface UpdateCreatorEventDto {
+  id: number;
+  creatorId: number;
+}
+
+@Controller('event')
 export class EventController {
   constructor(private readonly eventService: EventService) {}
 
-  @Get("matching/list")
+  @Get('matching/list')
   async getEventsWithMatchingFlagTrue() {
     return this.eventService.getEventsWithMatchingFlagTrue();
   }
 
-  @Get("detail/:id")
-  async getEventDetail(@Param("id") id: number) {
+  @Get('detail/:id')
+  async getEventDetail(@Param('id') id: number) {
     return this.eventService.getEventDetail(id);
+  }
+
+  @Post('')
+  async createEvent(@Body() event: CreateEventDto) {
+    return this.eventService.createEvent(event);
+  }
+
+  @Patch('creator-event/:eventId')
+  async updateCreatorEvents(
+    @Param('eventId') eventId: number,
+    @Body() creatorEvent: UpdateCreatorEventDto[],
+  ) {
+    return this.eventService.updateCreatorEvents(eventId, creatorEvent);
+  }
+
+  @Delete('creator-event/:id')
+  async deleteCreatorEvent(@Param('id') id: number) {
+    return this.eventService.deleteCreatorEvent(id);
   }
 }
--- api/src/modules/event/event.module.ts
+++ api/src/modules/event/event.module.ts
@@ -1,10 +1,13 @@
-import { Module } from "@nestjs/common";
-import { EventController } from "./event.controller";
-import { EventService } from "./event.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Event } from "src/entities/event.entity";
+import { Module } from '@nestjs/common';
+import { EventController } from './event.controller';
+import { EventService } from './event.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Event } from 'src/entities/event.entity';
+import { CreatorEvent } from 'src/entities/createrEvent.entity';
+import { Venue } from 'src/entities/venue.entity';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([Event])],
+  imports: [TypeOrmModule.forFeature([Event, CreatorEvent, Venue])],
   controllers: [EventController],
   providers: [EventService],
 })

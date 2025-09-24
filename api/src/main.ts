2de8b7da1623a7404fbd1a6f0231094c670ae80b
iesora <sorasorasora0718@gmail.com>
2025-09-25 01:23:04 +0900

    add origin (venue-matching-web)

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,16 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     credentials: true,
     origin: [
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'http://localhost:3002',
-      'https://five-zero-develop-admin-584693937256.asia-northeast1.run.app',
-      'https://five-zero-develop-web-584693937256.asia-northeast1.run.app',
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://localhost:3002",
+      "https://five-zero-develop-admin-584693937256.asia-northeast1.run.app",
+      "https://five-zero-develop-web-584693937256.asia-northeast1.run.app",
+      "https://venue-matching-web-584693937256.asia-northeast1.run.app",
     ],
   });
   await app.listen(process.env.PORT);
